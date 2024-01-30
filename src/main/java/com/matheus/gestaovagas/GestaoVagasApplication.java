5821e3a8e834d9ea23175f26254d9f3c2bf7e14b
Matheus Zweibrucker <matheuszweibrucker@gmail.com>
2024-01-30 19:02:31 -0300

    first commit

---
+++ src/main/java/com/matheus/gestaovagas/GestaoVagasApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.gestaovagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoVagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoVagasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheus/gestaovagas/GestaoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.gestaovagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
