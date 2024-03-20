ee477016b9a7b0e253e74a6a811b0af36df25512
Matheus Zweibrucker <matheuszweibrucker@gmail.com>
2024-03-20 18:08:35 -0300

    feat: error handling and connection database

--- src/main/java/com/matheus/gestaovagas/modules/candidate/CandidateEntity.java
+++ src/main/java/com/matheus/gestaovagas/modules/candidate/CandidateEntity.java
@@ -1,20 +1,41 @@
 package com.matheus.gestaovagas.modules.candidate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@Entity(name = "candidate")
 public class CandidateEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
+
+    @NotBlank(message = "Username is required")
     @Pattern(regexp = "^[a-zA-Z0-9]{5,}$", message = "Username must have at least 5 characters")
     private String username;
+
+    @Email(message = "Invalid email")
     private String email;
+
+    @Size(min = 8, message = "Password must have at least 8 characters")
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 }
--- src/main/java/com/matheus/gestaovagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/com/matheus/gestaovagas/modules/candidate/controllers/CandidateController.java
@@ -1,6 +1,8 @@
 package com.matheus.gestaovagas.modules.candidate.controllers;
 
 import com.matheus.gestaovagas.modules.candidate.CandidateEntity;
+import com.matheus.gestaovagas.modules.candidate.CandidateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/candidate")
 public class CandidateController {
 
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+
     @PostMapping("/")
-    public void create(@Validated @RequestBody CandidateEntity candidateEntity ) {
-        System.out.println("Candidato");
-        System.out.println(candidateEntity.getEmail());
+    public CandidateEntity create(@Validated @RequestBody CandidateEntity candidateEntity ) {
+        return this.candidateRepository.save(candidateEntity);
     }
 }
